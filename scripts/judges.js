7b60d9d7570ab189d2fcb4b3f50c6e0c605f7243
anastasia21112 <anad221122@gmail.com>
2021-11-21 15:41:49 -0700

    judges

---
+++ scripts/judges.js
@@ -0,0 +1,121 @@
+let membersObj = [
+    {
+        name: "Ekta Singh",
+        position: "United Engineers Lead",
+        linkedIn: "ekta-singh-106180147/",
+        home: "Edison, NJ",
+        team: "United Engineers",
+        src: "EktaSingh.jpg",
+        bio: "Ekta is a passionate advocate for equal opportunities among all and is a rising third-year student pursuing biological engineering with a minor in biotechnology at Purdue University in West Lafayette, Indiana. She wanted to join STEMchats because as a woman and person of color in STEM, she finds that there needs to be a higher representation of those groups leading in STEM, through increased awareness and literacy of STEM. Outside of STEMchats, you will find her watching documentaries, being a social butterfly, volunteering in her community, and traveling to explore new cultures and hidden artifacts around the world."
+    }, {
+        name: "Haoren Wen",
+        position: "United Engineers Lead",
+        linkedIn: null,
+        home: "Guangzhou, China",
+        team: "United Engineers",
+        src: "HaorenWen.jpg",
+        bio: "Haoran is an enthusiast in engineering and is a rising first year student at the Massachusetts Institute of Technology. He joined STEMchats in hopes of providing greater opportunities to first generation, low income students and help inspire a new wave of future engineers. In his free time, he enjoys exercising, playing the piano, and learning about new technologies."
+    }, {
+        name: "Lola Rustamova",
+        position: "United Engineers Lead",
+        linkedIn: "lolarustamova/",
+        home: "Brooklyn, NY",
+        team: ["United Engineers", "Speaker Series"],
+        src: "LolaRustamova.jpg",
+        bio: "Lola is a rising second-year student at Baruch College in New York City. She joined STEMchats because she's passionate about helping disadvantaged students discover and learn more about STEM and provide more opportunities to them. In her spare time she loves spending time with kids, reading, baking, and exploring New York City."
+    }, {
+        name: "Swan Yee Aung",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "Yangon, Burma",
+        team: "United Engineers",
+        src: "SwanYeeAung.jpg",
+        bio: "Swanyee is a rising first year student at New York University, pursuing his passion in computer science and engineering. He joined STEMchats in hopes of teaching younger students more about STEM and how fun it can be. In his free time, Swanyee likes to read books, meditate, do martial arts, or spend time with his family."
+    }, {
+        name: "Ayo Bravo",
+        position: "United Engineers Coordinator",
+        linkedIn: "ayotzin-bravo/",
+        home: "Salem, OR",
+        team: "United Engineers",
+        src: "AyoBravo.jpg",
+        bio: "Ayotzin, better known as Ayo, is a first-year student at the University of Pennsylvania who enjoys giving back to the community and is always willing to take on challenges. When asked about why he joined STEMchats, his response was simple \"because I believe in their goals and I'd like to help make this organization's vision a reality\". Ayo enjoys playing and watching soccer, spending time with his loved ones, and expanding his culinary palate."
+    }, {
+        name: "Teddy Smith",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "Stratford, CT",
+        team: "United Engineers",
+        src: "TeddySmith.jpg",
+        bio: "Teddy Smith is an aspiring rocket scientist and current senior at St Joseph High School in Trumbull, Connecticut. He joined STEMchats so he could share his passion for engineering with others and inspire those with interests in STEM. In his spare time, he enjoys building and flying racing drones, RC airplanes, launching model rockets, and snowboarding."
+    }, {
+        name: "Amy Xi",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "Philadelphia, PA",
+        team: "United Engineers",
+        src: "AmyXi.jpg",
+        bio: "Amy is a rising second-year student studying Biology at New York University. She joined STEMchats in order to inspire others to have a passion for learning about STEM and using it to help others. When she's not doing STEM-related things, she's probably listening to music or watching and analyzing figure skating and artistic gymnastics."
+    }, {
+        name: "Sarah Young",
+        position: "United Engineers Coordinator",
+        linkedIn: "sarah-young-955b451a1/",
+        home: "Philadelphia, PA",
+        team: "United Engineers",
+        src: "SarahYoung.jpg",
+        bio: "Sarah is a rising senior at Methacton High School. She is passionate about equitable education and programming. She joined STEMchats to help others develop knowledge in STEM and to share her passion for these fields.  In her free time, Sarah enjoys learning, trying new foods, and dancing."
+    }, {
+        name: "Emily Shang",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "Philadelphia, PA",
+        team: "United Engineers",
+        src: "EmilyShang.jpg",
+        bio: "Emily is passionate about advocating for equitable access to resources and educational opportunities both locally and globally. She is a rising senior at Lower Merion High School in the suburbs of Philadelphia. At STEMchats, she hopes to aid in evening out available educational STEM opportunities which are highly clustered amongst people traditionally represented in STEM and promote a habit of curiosity in learning STEM. In her free time, Emily likes to listen to music, compete with her friends in the NYTimes mini crossword, and fence."
+    }, {
+        name: "Justin Yoo",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "East Rutherford, NJ",
+        team: "United Engineers",
+        src: "JustinYoo.jpg",
+        bio: "Justin is a rising sophomore in the Engineering Academy at Bergen County Academies and is passionate about anything related with STEM. He joined STEMchats in order to pursue his passion in this field and to open up opportunities for low-income, minority students. He is Korean and is constantly trying to share his culture with others. During his free time, Justin loves to go on runs in the morning, play guitar and basketball, and watch movies with friends."
+    },
+    {
+        name: "Elaine Viloria",
+        position: "United Engineers Coordinator",
+        linkedIn: 'elaineviloria/',
+        home: "San Jose, CA",
+        team: "United Engineers",
+        src: "ElaineViloria.JPG",
+        bio: "Elaine is a rising second-year at UC Berkeley intending to major in environmental science and minor in materials science and engineering. She is passionate about creating a sustainable lifestyle that is more accessible to disadvantaged communities. She is heavily influenced by her own East Side San Jose community and Filipinx identity. Elaine joined STEMchats to foster her love for science and social justice and to support students like her that are exploring their interests in STEM fields. In her free time, she loves traveling, watching Parks and Rec, and spending time with her loved ones."
+    },
+    {
+        name: "Michelle Lee",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "Westminster, CA",
+        team: "United Engineers",
+        src: "MichelleLee.JPG",
+        bio: "Michelle is a rising senior at Westminster High School and is passionate about increasing the number of opportunities there are in the STEM field for less fortunate children. She believes that everyone should have a chance in trying their hand in the STEM field regardless of backgrounds. With STEMchats, she hopes to develop more accessible opportunities for communities that may not have a strong STEM background. In her free time, Michelle enjoys listening to music, spending time with friends and family, along with assembling figurines."
+    },
+    {
+        name: "Ryan Bui",
+        position: "United Engineers Coordinator",
+        linkedIn: null,
+        home: "Westminster, CA",
+        team: "United Engineers",
+        src: "RyanBui.JPG",
+        bio: "Ryan is a rising third-year student majoring in Mechanical Engineering at California State University of Long Beach. He joined STEMchats to give back to less fortunate communities as the one he grew up in, and to spread more awareness to the youth of this field. He is a proud Vietnamese-American who wants to help represent the minorities, and help influence their passions/dreams. On Ryan's free time, he likes to exercise, hangout with friends, play with his dog, and play sports. These sports are usually basketball, volleyball, and MMA. "
+    },
+    {
+        name: "Kiara Ortiz",
+        position: "United Engineers Coordinator",
+        linkedIn: "kiara-ortiz/",
+        home: "Bronx, NY",
+        team: "United Engineers",
+        src: "KiaraOrtiz.jpg",
+        bio: "Kiara is a rising freshman at Dartmouth College, aspiring to study Neuroscience and Public Policy. She is passionate about combating healthcare and education inequalities. She was a delegate at the 2018 Y20 summit, advocating for the Sustainable Development Goals. She has also conducted neuroscience research at Rockefeller University and mental health research at Baruch college. Through STEMchats, she aims to help create methods to promote accessibility to STEM education and open opportunities to deserving students. In her free time, Kiara likes to read sci-fi and create STEM raps."
+    }
+];
+
+window.onload = createMembers(membersObj);
---
+++ scripts/script.js
@@ -0,0 +1,118 @@
+const animate = document.getElementById("team_container");
+const team = document.getElementById("team");
+const url = "/images/headshots/";
+
+createMembers = (members) => {
+    members.forEach(function(value, i) {
+      //create card
+      createCard = () => {
+        divOne = document.createElement("div");
+        divOne.className = "col-md-4";
+        divTwo = document.createElement("div");
+        divTwo.className = "card teamcard";
+        img = document.createElement("img");
+        img.className = "modal-trigger card-img-top teamcard-img";
+        img.setAttribute("onClick", "removeAnimate()"); // remove aos animation/styling for modals
+        img.setAttribute("alt", members[i].name);
+        img.setAttribute("data-toggle", "modal");
+        img.setAttribute("src", url + members[i].src);
+        img.setAttribute("data-target", "#modal" + i); // the specific modal trigger
+        img.setAttribute("loading", "lazy");
+        divThree = document.createElement("div");
+        divThree.className = "card-body teamcard-body";
+        title = document.createElement("h3");
+        title.className = "card-title teamcard-title";
+        title.textContent = members[i].name;
+        desc = document.createElement("h3");
+        desc.className = "card-text teamcard-description";
+        desc.textContent = members[i].position;
+        divThree.appendChild(title);
+        divThree.appendChild(desc);
+        divTwo.appendChild(img);
+        divTwo.appendChild(divThree);
+        divOne.appendChild(divTwo);
+        return divOne;
+      }
+      //create modal
+      createModal = () => {
+        modal = document.createElement("div");
+        modal.id = "modal" + i; //the specific modal id
+        modal.className = "modal fade";
+        modal.setAttribute("tabindex", "-1");
+        // modal.setAttribute("role", "dialog");
+        modal.setAttribute("data-backdrop", "static");
+        modal.setAttribute("data-keyboard", "false");
+        modal.setAttribute("aria-labelledby", "Modal-label");
+        modal.setAttribute("aria-hidden", "true");
+        modalDialog = document.createElement("div");
+        modalDialog.className = "modal-dialog modal-dialog-centered";
+        // divTwo.setAttribute("role","document");
+        modalContent = document.createElement("div");
+        modalContent.className = "modal-content";
+        modalHeader = document.createElement("div");
+        modalHeader.className = "modal-header";
+        button = document.createElement("button");
+        button.className = "close";
+        button.setAttribute("onclick", "addAnimate()"); //add aos animation/styling again after exiting modal
+        button.setAttribute("type", "button");
+        button.setAttribute("data-dismiss", "modal");
+        button.setAttribute("aria-label", "Close");
+        span = document.createElement("span");
+        span.setAttribute("aria-hidden", "true");
+        span.innerHTML = "&times;"
+        button.appendChild(span);
+        modalHeader.appendChild(button);
+        modalBody = document.createElement("div");
+        modalBody.className = "modal-body";
+        container = document.createElement("div");
+        container.className = "container-fluid";
+        row = document.createElement("div");
+        row.className = "row";
+        col1 = document.createElement("div");
+        col1.className = "col-md-6 my-auto mx-auto";
+        img = document.createElement("img");
+        img.className = "img-fluid";
+        img.setAttribute("alt", members[i].name);
+        img.setAttribute("src", url + members[i].src);
+        col1.appendChild(img);
+        row.appendChild(col1);
+        col2 = document.createElement("div");
+        col2.className = "left col-lg-6 my-auto";
+        const name = document.createElement("h3");
+        name.className = "center name";
+        nameText = document.createTextNode(members[i].name);
+        name.appendChild(nameText); //create text node + element node for the name
+        //add linkedIn
+        if (typeof members[i].linkedIn === "string") {
+          linkedIn = "<a href=\"https://www.linkedin.com/in/" + members[i].linkedIn + "\" target=\"_blank\"><i class=\"fab fa-linkedin fa-2x\"></i></a>";
+          name.innerHTML += linkedIn;
+        }
+        position = document.createElement("p");
+        position.className = "center";
+        position.innerHTML = "<strong>" + members[i].position + "</strong>";
+        hometown = document.createElement("p");
+        hometown.className = "center";
+        hometown.innerHTML = "<strong>Hometown:</strong> ";
+        home = document.createElement("span");
+        home.textContent = members[i].home;
+        hometown.appendChild(home);
+        bio = document.createElement("p");
+        bio.textContent = members[i].bio;
+        col2.appendChild(name);
+        col2.appendChild(position);
+        col2.appendChild(hometown);
+        col2.appendChild(bio);
+        row.appendChild(col2);
+        container.appendChild(row);
+        modalBody.appendChild(container);
+        modalContent.appendChild(modalHeader);
+        modalContent.appendChild(modalBody);
+        modalDialog.appendChild(modalContent);
+        modal.appendChild(modalDialog);
+        return modal;
+      }
+      team.appendChild(createCard());
+      team.appendChild(createModal());
+    });
+}
+
