833d7d1640fe13b618ca9ae765ba1a11a064cd92
hannahz0 <85980913+hannahz0@users.noreply.github.com>
2022-05-13 23:46:35 -0700

    faq accordion

--- scripts/script.js
+++ scripts/script.js
@@ -154,3 +154,23 @@ hide = () => {
 }
 
 
+//faq accordion functionality
+
+var acc = document.getElementsByClassName("faq-question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
